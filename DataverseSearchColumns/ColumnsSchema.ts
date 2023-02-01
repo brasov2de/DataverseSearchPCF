a1040b54d57aa6ea81e55fd7bd10a5d722b7e5b2
Diana <diana.birkelbach@orbis.de>
2023-02-01 15:51:46 +0100

    init

---
+++ DataverseSearchColumns/ColumnsSchema.ts
@@ -0,0 +1,105 @@
+export const ColumnsSchema = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+      "Value": {
+        "type": "array",
+        "items": {
+            "type": "object",
+            "properties": {
+              "ColName": {"type": "string"},
+              "ColWidth": {"type": "integer"},
+              "ColDisplayName": {"type": "string"}
+            }
+          }        
+        }
+    }    
+};
+
+export const ColumnsDummy = {    
+    "Value": [
+        { 
+            "ColName": "accountnumber",
+            "ColWidth": 100,
+            "ColDisplayName": "No"
+        },
+        { 
+            "ColName": "name",
+            "ColWidth": 200,
+            "ColDisplayName": "Name"
+        },
+        { 
+            "ColName": "address1_city",
+            "ColWidth": 200,
+            "ColDisplayName": "City"
+        },
+        { 
+            "ColName": "telephone1",
+            "ColWidth": 200,
+            "ColDisplayName": "Telephone"
+        }
+    ]
+}
+  
+const QuickFindResponseExample = {
+    "Name": "resultset",
+    "Object": 10281,
+    "Rows": [
+        {
+            "Name": "result",
+            "Id": "diana_pcftesterid",
+            "Cells": [
+                {
+                    "Name": "diana_name",
+                    "Width": 300,
+                    "RelatedEntityName": "",
+                    "DisableMetaDataBinding": false,
+                    "LabelId": "",
+                    "IsHidden": false,
+                    "DisableSorting": false,
+                    "AddedBy": "",
+                    "Desc": "",
+                    "CellType": "",
+                    "ImageProviderWebresource": "",
+                    "ImageProviderFunctionName": ""
+                },
+                {
+                    "Name": "createdon",
+                    "Width": 125,
+                    "RelatedEntityName": "",
+                    "DisableMetaDataBinding": false,
+                    "LabelId": "",
+                    "IsHidden": false,
+                    "DisableSorting": false,
+                    "AddedBy": "",
+                    "Desc": "",
+                    "CellType": "",
+                    "ImageProviderWebresource": "",
+                    "ImageProviderFunctionName": ""
+                },
+                {
+                    "Name": "diana_technologycode",
+                    "Width": 100,
+                    "RelatedEntityName": "",
+                    "DisableMetaDataBinding": false,
+                    "LabelId": "",
+                    "IsHidden": false,
+                    "DisableSorting": false,
+                    "AddedBy": "",
+                    "Desc": "",
+                    "CellType": "",
+                    "ImageProviderWebresource": "",
+                    "ImageProviderFunctionName": ""
+                }
+            ],
+            "MultiObjectIdField": "",
+            "LayoutStyle": ""
+        }
+    ],
+    "CustomControlDescriptions": [],
+    "Jump": "diana_name",
+    "Select": true,
+    "Icon": true,
+    "Preview": true,
+    "IconRenderer": ""
+}
---
+++ DataverseSearchColumns/RetrieveView.ts
@@ -0,0 +1,18 @@
+
+/*
+const fetchXmlForQuickFind= (objectTypeCode : number) => ["<fetch top="50" >
+<entity name="savedquery" >
+  <attribute name="isquickfindquery" />
+  <attribute name="name" />
+  <attribute name="returnedtypecode" />
+  <attribute name="layoutjson" />
+  <filter>
+    <condition attribute="isquickfindquery" operator="eq" value="1" />
+    <condition attribute="returnedtypecode" operator="eq" value="10281" />
+  </filter>
+</entity>
+</fetch>"];
+
+export const retrieveView = (tableName : string, context: ComponentFramework.Context<IInputs>) : Promise<any> => {
+
+}*/
